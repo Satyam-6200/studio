bb8d13c269fa2ad590445a6c518810c5ad77e034
SATYAM KUMAR <mrsatyam080@gmail.com>
2025-09-15 21:32:37 +0000

    its sill showing loogin failed

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_AUTH_DOMAIN,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
 
