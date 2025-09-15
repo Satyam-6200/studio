cfce61694f27f5b13a9a5b3ede39303c42eddc87
SATYAM KUMAR <mrsatyam080@gmail.com>
2025-09-15 20:41:12 +0000

    get started and dot structure is getting over each other so make some di

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <main className="flex-grow">
         <section className="relative flex flex-col items-center justify-center overflow-x-hidden px-4 py-20 text-center">
           <div className="absolute inset-0 -z-10 h-full w-full bg-background bg-[radial-gradient(hsl(var(--muted))_1px,transparent_1px)] [background-size:16px_16px]"></div>
-          <div className="relative z-10">
+          <div className="relative z-10 mb-24">
             <h1 className="text-5xl font-bold font-headline bg-clip-text text-transparent bg-gradient-to-b from-foreground to-foreground/70 pb-4 md:text-7xl">
               Build UIs with CORRECTED UI
             </h1>
